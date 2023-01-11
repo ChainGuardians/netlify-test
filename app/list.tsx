b08815aac7eba24c8f245eb9df696671cad7a508
Adam Hodgson <hi@adamhodgson.com>
2023-01-11 11:20:15 +0000

    - small update to remove dynamic test

--- app/list.tsx
+++ app/list.tsx
@@ -3,25 +3,9 @@
 import styles from "./page.module.css";
 import Link from "next/link";
 
-const dynamicList = [
-    {
-        href: "/",
-        label: "Home"
-    },
-    {
-        href: "/test1",
-        label: "Page 1"
-    },
-    {
-        href: "/test2",
-        label: "Page 2"
-    }
-]
-
 const List = () => {
    return (
        <>
-           <h1>Non Dynamic List</h1>
            <ul className={styles.list}>
                <li>
                    <Link href={`/`}>
@@ -37,21 +21,14 @@ const List = () => {
                        <span>Page 2</span>
                    </Link>
                </li>
+
+               <li>
+                   <Link href={`/test3`}>
+                       <span>Page 3</span>
+                   </Link>
+               </li>
            </ul>
 
-           <h1>Dynamic List</h1>
-           <ul className={styles.list}>
-
-               {dynamicList.map((element, index) => {
-                   return (
-                       <li key={index}>
-                           <Link href={element.href}>
-                               <span>{element.label}</span>
-                           </Link>
-                       </li>
-                   )
-               })}
-           </ul>
        </>
    )
 }
---
+++ app/test3/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import List from "../list";
+import styles from "../page.module.css";
+
+const Page = () => {
+    return (
+        <main className={styles.main}>
+            <List />
+
+
+            Page 3
+        </main>
+    )
+}
+
+export default Page
